1483b877e97b4be0758ca6608e93ea32c9de298f
karl-no <karl.publicidade@gmail.com>
2023-04-04 15:21:18 -0300

    test: class Product tested

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "caneta",
+        "Pentel",
+        "2023-01-14",
+        "2033-01-14",
+        "110002055",
+        "em local fresco e longe do sol",
+    )
+
+    assert produto.__repr__() == (
+            "O produto caneta"
+            " fabricado em 2023-01-14"
+            " por Pentel com validade"
+            " até 2033-01-14"
+            " precisa ser armazenado em local fresco e longe do sol."
+    )
