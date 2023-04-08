9ca86f61beace53482251d84c55e698cad0fdb58
karl-no <karl.publicidade@gmail.com>
2023-04-08 20:31:50 -0300

    feat: inventory report thru main.py

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,13 @@
+from inventory_report.inventory.inventory import Inventory
+import sys
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    path = sys.argv[1]
+    type_report = sys.argv[2]
+
+    report = Inventory.import_data(path, type_report)
+    sys.stdout.write(report)
